5321a9cdb93b66ed1ed1d1dd8a2b60718e395078
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2024-06-11 14:01:40 +0800

    fix: revert changes

--- api-pacs/module/iam/infrastructure/service/IAMCommandService.go
+++ api-pacs/module/iam/infrastructure/service/IAMCommandService.go
@@ -86,31 +86,29 @@ func (service *IAMCommandService) ForgotTenantUserPassword(ctx context.Context, 
 
 // LoginTenantUser login tenant user by tenant
 func (service *IAMCommandService) LoginTenantUser(ctx context.Context, tenantID, idToken string) (string, error) {
-	uid := "BJGBhyBcvddIUfJAngwhQRIDbJl2"
+	firebaseAuth, err := service.FirebaseAdminSDK.App.Auth(ctx)
+	if err != nil {
+		log.Println(err)
+		return "", errors.New(apiError.FirebaseAuthError)
+	}
 
-	// firebaseAuth, err := service.FirebaseAdminSDK.App.Auth(ctx)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return "", errors.New(apiError.FirebaseAuthError)
-	// }
+	// tenant auth
+	tenantAuth, err := firebaseAuth.TenantManager.AuthForTenant(tenantID)
+	if err != nil {
+		log.Println(err)
+		return "", errors.New(apiError.FirebaseAuthError)
+	}
 
-	// // tenant auth
-	// tenantAuth, err := firebaseAuth.TenantManager.AuthForTenant(tenantID)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return "", errors.New(apiError.FirebaseAuthError)
-	// }
-
-	// authToken, err := tenantAuth.VerifyIDToken(ctx, idToken)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return "", errors.New(apiError.UnauthorizedAccess)
-	// }
+	authToken, err := tenantAuth.VerifyIDToken(ctx, idToken)
+	if err != nil {
+		log.Println(err)
+		return "", errors.New(apiError.UnauthorizedAccess)
+	}
 
 	// persist to token session cache
 	sessionToken := generateID()
 
-	user, err := service.UserQueryServiceInterface.GetTenantUserByID(ctx, tenantID, uid)
+	user, err := service.UserQueryServiceInterface.GetTenantUserByID(ctx, tenantID, authToken.UID)
 	if err != nil {
 		return "", err
 	}
