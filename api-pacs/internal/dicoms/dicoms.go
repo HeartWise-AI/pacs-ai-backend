a2651769cfa0fa4000e394f0589d871cf8da9230
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2024-08-20 08:14:35 +0800

    refactor: move parsing age to dicom utils

--- api-pacs/internal/dicoms/dicoms.go
+++ api-pacs/internal/dicoms/dicoms.go
@@ -2,7 +2,10 @@ package dicoms
 
 import (
 	"fmt"
+	"log"
 	"reflect"
+	"strings"
+	"time"
 
 	"github.com/suyashkumar/dicom"
 	"github.com/suyashkumar/dicom/pkg/frame"
@@ -10,7 +13,7 @@ import (
 )
 
 // DICOMToInstances convert DICOM to instances
-func DICOMToInstances(dataset *dicom.Dataset) ([][][][]int, error) {
+func DICOMToInstances(dataset dicom.Dataset) ([][][][]int, error) {
 	pixelDataElement, err := dataset.FindElementByTag(tag.PixelData)
 	if err != nil {
 		return nil, fmt.Errorf("error getting pixel data: %w", err)
@@ -35,6 +38,37 @@ func FindMaxProbabilityIndex(dataset [][]float64) int {
 	}
 
 	return maxIndex
+}
+
+// ParseAge parse age from DICOM dataset
+func ParseAge(dataset dicom.Dataset) (int, error) {
+	ageElement, _ := dataset.FindElementByTag(tag.PatientBirthDate)
+	birthDateStr := ageElement.Value.String()
+	age, err := calculateAge(birthDateStr)
+	if err != nil {
+		return 0, err
+	}
+
+	return age, nil
+}
+
+func calculateAge(birthDateStr string) (int, error) {
+	birthDateStr = strings.Trim(birthDateStr, "[] \t\n\r")
+	birthDate, err := time.Parse("20060102", birthDateStr)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	now := time.Now()
+	age := now.Year() - birthDate.Year()
+
+	// adjust age if birthday hasn't occurred this year
+	if now.YearDay() < birthDate.YearDay() {
+		age--
+	}
+
+	return age, nil
 }
 
 func convertFramesToBase64Maps(frames []*frame.Frame) [][][][]int {
--- api-pacs/module/prediction/infrastructure/service/PredictionCommandService.go
+++ api-pacs/module/prediction/infrastructure/service/PredictionCommandService.go
@@ -5,11 +5,8 @@ import (
 	"context"
 	"errors"
 	"log"
-	"strings"
-	"time"
 
 	"github.com/suyashkumar/dicom"
-	"github.com/suyashkumar/dicom/pkg/tag"
 
 	inferenceAPITypes "api-pacs/infrastructures/providers/api/inference/types"
 	orthancAPITypes "api-pacs/infrastructures/providers/api/orthanc/types"
@@ -41,15 +38,14 @@ func (service *PredictionCommandService) Predict(ctx context.Context, queryID st
 	}
 
 	// get age
-	ageElement, _ := dataset.FindElementByTag(tag.PatientBirthDate)
-	birthDateStr := ageElement.Value.String()
-	age, err := calculateAge(birthDateStr)
+	age, err := dicomUtils.ParseAge(dataset)
 	if err != nil {
+		log.Println(err)
 		return types.PredictionResult{}, errors.New(apiError.DICOMParseError)
 	}
 
 	// convert DICOM to instances
-	instances, err := dicomUtils.DICOMToInstances(&dataset)
+	instances, err := dicomUtils.DICOMToInstances(dataset)
 	if err != nil {
 		log.Println(err)
 		return types.PredictionResult{}, errors.New(apiError.DICOMParseError)
@@ -97,23 +93,4 @@ func (service *PredictionCommandService) Predict(ctx context.Context, queryID st
 		LVEF:   detectedLVEF,
 		Age:    age,
 	}, nil
-}
-
-func calculateAge(birthDateStr string) (int, error) {
-	birthDateStr = strings.Trim(birthDateStr, "[] \t\n\r")
-	birthDate, err := time.Parse("20060102", birthDateStr)
-	if err != nil {
-		log.Println(err)
-		return 0, err
-	}
-
-	now := time.Now()
-	age := now.Year() - birthDate.Year()
-
-	// adjust age if birthday hasn't occurred this year
-	if now.YearDay() < birthDate.YearDay() {
-		age--
-	}
-
-	return age, nil
 }
