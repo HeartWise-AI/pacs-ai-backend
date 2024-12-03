dc50c27cfd60fa017b8204450a820f723f8e1a53
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2024-12-03 15:05:24 +0800

    test: print path

--- api-pacs/interfaces/http/rest/proxies/dockerinference/DockerInferenceProxy.go
+++ api-pacs/interfaces/http/rest/proxies/dockerinference/DockerInferenceProxy.go
@@ -32,7 +32,7 @@ func (proxy *DockerInferenceProxy) AppViewerProxy() http.HandlerFunc {
 		}
 
 		// construct the target container URL
-		target := fmt.Sprintf("http://%s/app/viewer/", containerName)
+		target := fmt.Sprintf("http://%s", containerName)
 		targetURL, err := url.Parse(target)
 		if err != nil {
 			response := viewmodels.HTTPResponseVM{
@@ -49,19 +49,15 @@ func (proxy *DockerInferenceProxy) AppViewerProxy() http.HandlerFunc {
 		// create the reverse proxy
 		reverseProxy := httputil.NewSingleHostReverseProxy(targetURL)
 
-		// modify the request path to only strip the proxy prefix but keep the rest of the path
-		prefix := fmt.Sprintf("/api/proxy/%s/app/viewer/", containerName)
-		if strings.HasPrefix(r.URL.Path, prefix) {
-			r.URL.Path = r.URL.Path[len(prefix):]
-			// ensure there's always a leading slash
-			if !strings.HasPrefix(r.URL.Path, "/") {
-				r.URL.Path = "/" + r.URL.Path
-			}
-		}
-
-		r.Host = targetURL.Host
+		// modify the request path to strip the prefix
+		r.URL.Path = strings.TrimPrefix(r.URL.Path, fmt.Sprintf("/proxy/%s/app/viewer", containerName))
+		r.URL.Host = targetURL.Host
+		r.URL.Scheme = targetURL.Scheme
 		r.Header.Set("X-Forwarded-Host", r.Host)
 		r.Header.Set("X-Forwarded-For", r.RemoteAddr)
+		r.Host = targetURL.Host
+
+		fmt.Println("AFTER:", r.URL.Host, r.URL.Path, r.URL.String())
 
 		// serve the reverse proxy
 		reverseProxy.ServeHTTP(w, r)
