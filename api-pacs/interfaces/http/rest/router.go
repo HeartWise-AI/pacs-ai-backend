4be7efa39e01edd5265a80800a78d9aa9400a033
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2025-05-25 22:32:25 +0800

    build: bump version

--- api-pacs/interfaces/http/rest/router.go
+++ api-pacs/interfaces/http/rest/router.go
@@ -74,7 +74,7 @@ func (router *router) InitRouter() *chi.Mux {
 			Success: true,
 			Message: "alive",
 			Data: map[string]interface{}{
-				"version": "v0.19.0-beta",
+				"version": "v0.20.0-beta",
 			},
 		}
 
