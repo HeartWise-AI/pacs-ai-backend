ece3275599134b96f03122d9246b07cda8702dd8
jacques <jdelfrate504@gmail.com>
2026-01-26 12:54:42 -0500

    Enhance CustomPredictionService with error handling and debugging prints. Added check for empty predictions, improved DICOM processing logs, and ensured proper handling of multi-frame videos.

--- model-examples/DeepCoro_CLIP/procedure_view_classifier/logic.py
+++ model-examples/DeepCoro_CLIP/procedure_view_classifier/logic.py
@@ -92,8 +92,12 @@ class CustomPredictionService(BasePredictionService):
         Returns:
             DataFrame with added 'status' column
         """
-        print("Assigning procedure status based on PCI timing...")        
-        df = pd.DataFrame(predictions, columns=predictions[0].keys())
+        print("Assigning procedure status based on PCI timing...")
+        
+        if not predictions:
+            return []
+        
+        df = pd.DataFrame(predictions)
 
         # 1. Parse and sort by time (safe)
         df["series_time_dt"] = pd.to_datetime(
@@ -152,6 +156,7 @@ class CustomPredictionService(BasePredictionService):
                                             
                         dicom = pydicom.dcmread(BytesIO(dicom_data))
                         dicoms.append(dicom)
+                        print(str(dicom.SeriesInstanceUID))
                         dicom_names.append(str(dicom.SeriesInstanceUID))
                         
                     except Exception as e:
@@ -167,13 +172,14 @@ class CustomPredictionService(BasePredictionService):
             }
         
         try:
+            print(f"len(dicoms): {len(dicoms)}, len(dicom_names): {len(dicom_names)}")
             structured_predictions: dict[str, dict[str, np.ndarray]] = self._run_inference(dicoms, dicom_names)
         except Exception as e:
             print(f"Error in _run_inference: {e}")
             return {
                 "predictions": []
             }
-        
+        print(f"structured_predictions: {structured_predictions}")
         try:
             structured_predictions = self._assign_procedure_status(predictions=structured_predictions)
         except Exception as e:
@@ -286,6 +292,7 @@ class CustomPredictionService(BasePredictionService):
                 
                 video = self._process_dicom(dicom)
                 if video is None:
+                    print(f"Skipping DICOM {dicom_name}: not a multi-frame video (ndim <= 2)")
                     continue
                 
                 video_stack.append(video)
@@ -298,6 +305,7 @@ class CustomPredictionService(BasePredictionService):
             if not video_stack:
                 return []
             video_stack = torch.stack(video_stack).to('cuda' if torch.cuda.is_available() else 'cpu')
+            
             with torch.no_grad():
                 output_stack: dict[str, torch.Tensor] = CustomPredictionService.models['vaso_vision'](video_stack)
                 
