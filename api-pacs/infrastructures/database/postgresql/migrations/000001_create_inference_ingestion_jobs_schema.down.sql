627746d83a359d4bcdc3d65e336c43d2642a7486
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2026-04-04 15:29:30 +0800

    feat(api,inference): migration fixes

--- api-pacs/infrastructures/database/postgresql/migrations/000001_create_inference_ingestion_jobs_schema.down.sql
+++ api-pacs/infrastructures/database/postgresql/migrations/000001_create_inference_ingestion_jobs_schema.down.sql
@@ -1,3 +1,4 @@
+DROP TRIGGER IF EXISTS trigger_update_inference_ingestion_jobs_updated_at ON inference_ingestion_jobs;
+DROP FUNCTION IF EXISTS func_update_updated_at();
 DROP TABLE IF EXISTS inference_ingestion_jobs;
 DROP TYPE IF EXISTS inference_ingestion_job_status;
-DROP FUNCTION IF EXISTS func_update_updated_at;
