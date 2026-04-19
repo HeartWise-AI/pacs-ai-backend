924e8757681fcadf46c84dd4e50ddce2e4f36438
JacquesDelfrate <jdelfrate504@gmail.com>
2026-04-19 08:49:59 -0400

    feat(database): enhance InferenceIngestionJob schema by adding fields for stability, recent window, and study time parameters

--- api-pacs/module/inference/infrastructure/repository/types/dto.go
+++ api-pacs/module/inference/infrastructure/repository/types/dto.go
@@ -55,7 +55,11 @@ type CreateInferenceIngestionJob struct {
 	ModelName              string
 	ModelVersion           string
 	Modalities             []string
-	IntervalInMinutes      uint
+	StabilityMinutes       uint
+	RecentWindowMinutes    uint
+	MissingPollsThreshold  uint
+	StudyTimeStart         *string
+	StudyTimeEnd           *string
 	ScheduleStartTimestamp time.Time
 	ScheduleEndTimestamp   time.Time
 	Status                 entity.InferenceIngestionJobStatus
@@ -82,7 +86,11 @@ type UpdateInferenceModel struct {
 type UpdateInferenceIngestionJob struct {
 	ID                     string
 	Modalities             []string
-	IntervalInMinutes      uint
+	StabilityMinutes       uint
+	RecentWindowMinutes    uint
+	MissingPollsThreshold  uint
+	StudyTimeStart         *string
+	StudyTimeEnd           *string
 	ScheduleStartTimestamp time.Time
 	ScheduleEndTimestamp   time.Time
 }
